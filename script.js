c26d94551e1a7e916de6a4b015e3edc8e5db667f
komunizmo <109455752+komunizmo@users.noreply.github.com>
2023-06-27 11:54:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<!-- HTML code snippet -->
+<!-- Create a responsive portfolio template with HTML, CSS, and JavaScript -->
+
+<!-- Example code: -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <!-- Add navigation menu items -->
+    </nav>
+  </header>
+  
+  <main>
+    <!-- Add sections for About, Projects, Skills, etc. -->
+  </main>
+
+  <footer>
+    <!-- Add footer content -->
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+</html>
